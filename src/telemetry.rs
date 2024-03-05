cbc5b988658b9bd0930cfa81ccf7f11ad7f12243
Sanu Shilshad <sanu.shilshad@acelrtech.com>
2024-03-05 19:34:19 +0000

    feat: move jeager implementaton to seperate function + remove jeager temporarily

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -18,6 +18,46 @@ pub fn get_subscriber<Sink>(
 where
     Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
 {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let format_layer = fmt::Layer::default()
+        .with_ansi(true)
+        .with_writer(sink)
+        .with_filter(LevelFilter::INFO);
+    Registry::default().with(env_filter).with(format_layer)
+}
+
+// Subscriber
+pub fn get_json_subscriber<Sink>(
+    _name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(_name, sink);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+}
+
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
+    set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+pub fn get_subscriber_with_jeager<Sink>(
+    _name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Send + Sync
+where
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     // let tracer = opentelemetry_jaeger::new_agent_pipeline()
     //     .with_endpoint(std::env::var("JAEGER_ENDPOINT").unwrap_or("localhost:4318".to_string()))
     //     .with_service_name("rust_test".to_string())
@@ -42,27 +82,4 @@ where
         .with(telemetry_layer)
         .with(env_filter)
         .with(format_layer)
-}
-
-// Subscriber
-pub fn get_json_subscriber<Sink>(
-    _name: String,
-    env_filter: String,
-    sink: Sink,
-) -> impl Subscriber + Send + Sync
-where
-    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
-{
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(_name, sink);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
-    LogTracer::init().expect("Failed to set logger");
-    set_global_default(subscriber).expect("Failed to set subscriber");
 }
